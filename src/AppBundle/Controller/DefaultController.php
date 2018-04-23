5e886d6d7182a573827e8dec0c4cf6fd655944b6
David Pellecuer <pellecuer.david@gmail.com>
2018-04-23 14:38:36 +0200

    maj flyaround

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,33 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+
+//ajouté par David
+
+    /**
+     * @Route("/hello", name="hello",)
+
+    *public function helloAction(Request $request)
+    {
+
+        return new Response('Bonjour');
+    }
+     */
+
+
+
+//ajouté par David2
+
+    /**
+     * @Route("/hello/{name}", name="hello",)
+     */
+    public function helloAction($name)
+    {
+
+        return
+            $this->render('default/hello.html.twig', array("name"=>$name)
+            );
     }
 }
